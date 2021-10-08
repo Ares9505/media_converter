ca786d614618016b2ca2d031a7d38f71e7828464
Ares9505 <arielic1995@gmail.com>
2021-10-08 12:27:30 -0400

    Generating key file

--- convertert.py
+++ convertert.py
@@ -3,12 +3,13 @@ from pydub.utils import mediainfo
 from pathlib import Path
 import os
 import logging
+from cryptography.fernet import Fernet
 
 
 
-def main():
+def convert_media():
 	base_dir = os.getcwd()
-	for paths,folders ,files in os.walk(base_dir):
+	for paths,_,files in os.walk(base_dir + "/audio/"):
 		for file in files:
 			storage_dir = base_dir + "/converted/" + str(Path(file).stem) + ".ogg"
 			if Path(file).suffix == ".mp3":	
@@ -17,12 +18,26 @@ def main():
 
 			if Path(file).suffix == ".flac":
 				audio = AudioSegment.from_file(os.path.join(paths,file), "flac")				
-				audio_converted = audio.export(storage_dir, format = "ogg", bitrate="128000")
+				audio_converted = audio.export(storage_dir, format = "ogg", bitrate="96000")
 				# print(mediainfo(base_dir + "/converted/" + str(Path(file).stem) + ".ogg")['bit_rate'])
 
+def generate_key():
+	key = Fernet.generate_key()
+	with open("mykey.key","wb") as key_file:
+		key_file.write(key) 
+
+
+def main():
+	convert_media()
 
 
 if __name__ == "__main__":
 	logging.basicConfig(level=logging.INFO)
-	main()
+	generate_key()
 	
+'''Tareas:
+
+*Comprobar que el formato sea ogg VORBIS
+*Encriptar un archivo
+*Subir archivos a dropbox
+'''
