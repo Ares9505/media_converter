fbc9bcf1cbe06d6b6ddc4741ff4c6725ea70d18b
Ares9505 <arielic1995@gmail.com>
2021-10-08 12:11:40 -0400

    Setting gitignore

--- convertert.py
+++ convertert.py
@@ -13,7 +13,7 @@ def main():
 			storage_dir = base_dir + "/converted/" + str(Path(file).stem) + ".ogg"
 			if Path(file).suffix == ".mp3":	
 				audio = AudioSegment.from_mp3( os.path.join(paths,file))			
-				audio_converted = audio.export(storage_dir, format = "ogg", bitrate="128000")
+				audio_converted = audio.export(storage_dir, format = "ogg", bitrate="96000")
 
 			if Path(file).suffix == ".flac":
 				audio = AudioSegment.from_file(os.path.join(paths,file), "flac")				
